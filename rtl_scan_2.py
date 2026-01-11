8d3b7bf791bef38f475fc96c3b189500d5a24f4b
3DCentral <57771524+3DCentral@users.noreply.github.com>
2026-01-10 23:28:46 -0500

    Add files via upload

--- rtl_scan_2.py
+++ rtl_scan_2.py
@@ -1,5 +1,7 @@
 from rtlsdr import RtlSdr
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # Use non-interactive backend
 import matplotlib.pyplot as plt
 import sys
 from scipy import signal
@@ -15,22 +17,16 @@ end_freq = int(float(sys.argv[2]))
 print(start_freq)
 print(end_freq)
 middle_freq = (end_freq + start_freq) / 2
-sampling_freq = sample_rate #(end_freq - start_freq) / sdr.sample_rate * sdr.sample_rate
+sampling_freq = sample_rate
 total_scan_count  = int((end_freq - start_freq) / sample_rate)
 print(middle_freq)
 print(sampling_freq)
 print(total_scan_count)
 sdr.center_freq = int(start_freq+sample_rate/2)
 
-#sdr.center_freq = start_freq + sdr.sample_rate/2 #161e6   # Hz
 sdr.freq_correction = 60  # PPM
-#print(sdr.valid_gains_db)
-sdr.gain = 4 #49.6
+sdr.gain = 4
 print(sdr.gain)
-
-
-# 5% overlap
-#sdr.sample_rate = sdr.sample_rate * 1.1
 
 fft_size = 512
 num_rows = 500
@@ -40,19 +36,16 @@ x = np.array([])
 all_frequencies = []
 all_psd_data = []
 
-#z = sdr.read_samples(fft_size*num_rows)
-
 print("Size of the array:", np.prod(x.shape))
 num_scans = 0
-for f in range(int(start_freq+sample_rate/2),int(end_freq-sample_rate/2),int(sample_rate)):
+
+for f in range(int(start_freq+sample_rate/2),int(end_freq-sample_rate/2)+int(sample_rate),int(sample_rate)):
     start = f - sample_rate/2
     end = f + sample_rate/2
     print("scanning ",f,start,end)
     sdr.center_freq = f
     y = sdr.read_samples(fft_size*num_rows)
-    #y = y - np.mean(y)  # Subtract the mean value, try to minimize center frequency bias
     frequencies, psd = signal.welch(y, fs=sample_rate, window='boxcar')
-    #frequencies, psd = signal.welch(y, fs=sample_rate, window='hann')
 
     target_f = f
     print("target ", target_f)
@@ -61,7 +54,7 @@ for f in range(int(start_freq+sample_rate/2),int(end_freq-sample_rate/2),int(sam
     # Define a window around the center frequency
     window_size = 10  # Adjust as needed
     window_indices = np.arange(center_frequency_index - window_size, center_frequency_index + window_size + 1)
-    window_indices = np.clip(window_indices, 0, len(psd) - 1)  # Ensure indices are within bounds
+    window_indices = np.clip(window_indices, 0, len(psd) - 1)
 
     # Calculate the average PSD within the window
     average_psd = np.mean(psd[window_indices])
@@ -78,103 +71,123 @@ for f in range(int(start_freq+sample_rate/2),int(end_freq-sample_rate/2),int(sam
     progress_frequencies, progress_psd_data = zip(*sorted(zip(all_frequencies, all_psd_data)))
 
     # Convert to NumPy arrays
-    progress_frequencies = np.array(progress_frequencies)  # Convert to NumPy array
-    progress_psd_data = np.array(progress_psd_data)  # Convert to NumPy array
+    progress_frequencies = np.array(progress_frequencies)
+    progress_psd_data = np.array(progress_psd_data)
 
     # Define outlier threshold in dB
-    outlier_threshold_db = -200  # Adjust as needed
+    outlier_threshold_db = -200
 
     # Create outlier mask
     outlier_mask = 10 * np.log10(progress_psd_data) < outlier_threshold_db
 
     # Remove outliers
-    non_outlier_indices = np.where(~outlier_mask)[0]  # Use np.where to get indices
+    non_outlier_indices = np.where(~outlier_mask)[0]
     filtered_psd = progress_psd_data[non_outlier_indices]
-    filtered_frequencies = progress_frequencies[non_outlier_indices]  # Use indices for frequencies as well
-
-
+    filtered_frequencies = progress_frequencies[non_outlier_indices]
 
     num_scans = num_scans + 1
-    plt.clf()
-    plt.rcParams['axes.facecolor'] = 'black'
-    avg_f = (start_freq + end)/2
-    print(avg_f)
-    #plt.psd(x, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6, color="yellow")
-    #plt.psd(x, Fs=sampling_freq*total_scan_count/1e6, Fc=middle_freq/1e6, color="yellow", window=None)
-    #plt.psd(x, Fs=sampling_freq*num_scans/1e6, Fc=avg_f/1e6, color="yellow", window=None)
-    plt.xlabel("MHz", color="yellow")
-    plt.ylabel("dB", color="yellow")
-    plt.tick_params(colors='yellow', which='both')
-    plt.plot(filtered_frequencies, 10*np.log10(filtered_psd),color="yellow")
-    plt.savefig("/home/tricorder/rpi_lcars-master/spectrum_progress_"+str(num_scans)+".png", bbox_inches='tight',dpi=110, facecolor="black")
-    plt.savefig("/home/tricorder/rpi_lcars-master/spectrum.png", bbox_inches='tight',dpi=110, facecolor="black")
+    
+    # Create figure with proper layout for labels
+    fig = plt.figure(figsize=(6.4, 3.36), dpi=100)  # 640x336 pixels
+    
+    # Create axes that leaves room for x-axis labels at bottom
+    # [left, bottom, width, height] in figure coordinates
+    ax = plt.axes([0, 0.08, 1, 0.92])  # Leave 8% at bottom for labels
+    
+    # Remove top, right, and left spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    
+    # Remove y-axis completely (no labels, no ticks)
+    ax.set_yticks([])
+    ax.set_ylabel('')
+    
+    # Set background to black
+    fig.patch.set_facecolor('black')
+    ax.set_facecolor('black')
+    
+    # Plot the spectrum data
+    ax.plot(filtered_frequencies, 10*np.log10(filtered_psd), color="yellow", linewidth=2)
+    
+    # Set x-limits to exact frequency range (no padding)
+    ax.set_xlim(start_freq, end_freq)
+    
+    # Set x-axis labels in MHz
+    ax.tick_params(axis='x', colors='yellow', labelsize=9)
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: '{:.1f}'.format(x/1e6)))
+    
+    # Set background color for tick labels
+    for label in ax.get_xticklabels():
+        label.set_color('yellow')
+    
+    # Save progress image
+    plt.savefig("/home/tricorder/rpi_lcars-master/spectrum_progress_"+str(num_scans)+".png", 
+                dpi=100, facecolor="black", bbox_inches='tight', pad_inches=0)
+    plt.savefig("/home/tricorder/rpi_lcars-master/spectrum.png", 
+                dpi=100, facecolor="black", bbox_inches='tight', pad_inches=0)
+    plt.close()
 
 print(num_scans)
 print(total_scan_count)
 print("Size of the array:", np.prod(x.shape))
-#x = sdr.read_samples(fft_size*num_rows) # get all the samples we need for the spectrogram
 
 sdr.close()
 
+# Final processing
 all_frequencies, all_psd_data = zip(*sorted(zip(all_frequencies, all_psd_data)))
 all_frequencies = np.array(all_frequencies)
 all_psd_data = np.array(all_psd_data)
-#smoothed_psd = ndimage.uniform_filter1d(merged_psd, size=5)
 
 # Define outlier threshold in dB
-outlier_threshold_db = -200  # Adjust as needed
+outlier_threshold_db = -200
 
 # Create outlier mask
 outlier_mask = 10 * np.log10(all_psd_data) < outlier_threshold_db
 
 # Remove outliers
-non_outlier_indices = np.where(~outlier_mask)[0]  # Use np.where to get indices
+non_outlier_indices = np.where(~outlier_mask)[0]
 filtered_psd = all_psd_data[non_outlier_indices]
-filtered_frequencies = all_frequencies[non_outlier_indices]  # Use indices for frequencies as well
+filtered_frequencies = all_frequencies[non_outlier_indices]
 
+# Create final figure with frequency labels
+fig = plt.figure(figsize=(6.4, 3.36), dpi=100)  # 640x336 pixels
 
-plt.figure()
+# Create axes that leaves room for x-axis labels at bottom
+ax = plt.axes([0, 0.08, 1, 0.92])  # Leave 8% at bottom for labels
 
+# Remove top, right, and left spines
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
 
-#plt.clf()
-plt.rcParams['axes.facecolor'] = 'black'
-#plt.psd(x, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6, color="yellow")
-#plt.psd(x, Fs=sampling_freq*total_scan_count/1e6, Fc=middle_freq/1e6, color="yellow", window=None)
-plt.xlabel("MHz", color="yellow")
-plt.ylabel("dB", color="yellow")
-plt.tick_params(colors='yellow', which='both')
+# Remove y-axis completely (no labels, no ticks)
+ax.set_yticks([])
+ax.set_ylabel('')
 
-plt.plot(filtered_frequencies, 10*np.log10(filtered_psd),color="yellow")
-plt.savefig("/home/tricorder/rpi_lcars-master/spectrum.png", bbox_inches='tight',dpi=110, facecolor="black")
+# Set background to black
+fig.patch.set_facecolor('black')
+ax.set_facecolor('black')
 
+# Plot final spectrum
+ax.plot(filtered_frequencies, 10*np.log10(filtered_psd), color="yellow", linewidth=2)
 
+# Set x-limits to exact frequency range (no padding)
+ax.set_xlim(start_freq, end_freq)
 
+# Set x-axis labels in MHz
+ax.tick_params(axis='x', colors='yellow', labelsize=9)
+ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: '{:.1f}'.format(x/1e6)))
 
-#plt.rcParams['axes.facecolor'] = 'black'
-#plt.psd(x, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6, color="yellow")
-#plt.psd(z, Fs=sdr.sample_rate*6/1e6, Fc=sdr.center_freq/1e6 - 3*sdr.sample_rate/1e6, color="yellow", window=None)
-##plt.xlabel("MHz", color="yellow")
-#plt.ylabel("dB", color="yellow", labelpad=5)  # Keep or adjust as needed
-#plt.tick_params(colors='yellow', which='both')
-##plt.tick_params(axis='y', colors='yellow', direction='in', pad=-15)  # Adjust y-axis tick parameters
-#plt.tick_params(axis='x', colors='yellow', pad=0)  # Adjust y-axis tick parameters
-#plt.tight_layout() #This line was added
-#plt.gca().yaxis.set_label_coords(0.05, 0.5) # Adjust y-axis label position
-#plt.savefig("/home/tricorder/rpi_lcars-master/spectrum.png", bbox_inches='tight',dpi=108, facecolor="black", pad_inches=0)
+# Set background color for tick labels
+for label in ax.get_xticklabels():
+    label.set_color('yellow')
 
-#spectrogram = np.zeros((num_rows, fft_size))
-#for i in range(num_rows):
-#    spectrogram[i,:] = 10*np.log10(np.abs(np.fft.fftshift(np.fft.fft(x[i*fft_size:(i+1)*fft_size])))**2)
+# Save final image
+plt.savefig("/home/tricorder/rpi_lcars-master/spectrum.png", 
+            dpi=100, facecolor="black", bbox_inches='tight', pad_inches=0)
+plt.close()
 
-# extent is for the x and y label ranges
-#extent = [(sdr.center_freq + sdr.sample_rate/-2)/1e6,
-#            (sdr.center_freq + sdr.sample_rate/2)/1e6,
-#            len(x)/sdr.sample_rate, 0]
-#plt.imshow(spectrogram, aspect='auto', extent=extent)
-#plt.xlabel("Frequency [MHz]")
-#plt.ylabel("Time [s]")
-
-#plt.savefig("/home/tricorder/rpi_lcars-master/spectrum.png", bbox_inches='tight')
-#cairosvg.svg2png(url="../pi/spectrum.svg", write_to="../pi/spectrum.png", scale=3.0)
-
-#plt.show()
+print("Scan complete!")
